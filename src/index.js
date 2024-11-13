e54464c1623303dfe36b32c736e69b30c97bdc05
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2024-11-13 18:30:29 +0530

    "Refactored chat application code, updated package.json, and modified index.html and real_chat.html files."

--- src/index.js
+++ src/index.js
@@ -22,17 +22,14 @@ io.on('connection', (socket) => {
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(process.cwd(), "public")));
 
-// Handle the root route to serve the 'index.html' file
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(process.cwd(), "public", "index.html"));
 });
 
-// Start the server
+
 server.listen(9000, () => {
   console.log("Server is started at port: 9000");
 });
 
 
-// process.cwd() ka matlab hai Current Working Directory. Jab aap Node.js ka code run karte hain, toh ye function aapko us folder ka path deta hai jahan se aapka Node.js process start hua tha. Yeh bahut zaroori hota hai, kyunki kabhi kabhi humare project ka root directory alag hota hai, aur hum chahte hain ki file paths ko relative tarike se handle karein.
-
-// fronted se connect hoga to client ki information hogi ar har socket ki id hoti hain
